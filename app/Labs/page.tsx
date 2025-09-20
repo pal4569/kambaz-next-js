028ee0de710620833426d3d9fe3671ae0b053263
pal4569 <michaelfc8841@gmail.com>
2025-09-19 20:45:34 -0400

    add github link, first & last name, and section

--- app/Labs/page.tsx
+++ app/Labs/page.tsx
@@ -3,6 +3,8 @@ export default function Labs() {
  return (
    <div id="wd-labs">
      <h1>Labs</h1>
+     <h2>Michael Callahan</h2>
+     <h3>Section 1</h3>
      <ul>
        <li>
          <Link href="/Labs/Lab1" id="wd-lab1-link">
@@ -15,6 +17,9 @@ export default function Labs() {
        <li>
          <Link href="/Labs/Lab3" id="wd-lab3-link">
            Lab 3: JavaScript Fundamentals </Link>
+       </li>
+       <li>
+         <a href="https://github.com/pal4569/kambaz-next-js" id="wd-github"> Github </a>
        </li>
      </ul>
    </div>
