a86f83b8cc2e5bb5193f981c955b072f39250b43
pal4569 <michaelfc8841@gmail.com>
2025-09-29 20:00:52 -0400

    A2 55 - 65

--- app/Labs/Lab2/page.tsx
+++ app/Labs/Lab2/page.tsx
@@ -1,5 +1,6 @@
 import './index.css'
 import "bootstrap/dist/css/bootstrap.min.css";
+import Container from 'react-bootstrap/Container';
 import ForegroundColors from "./ForegroundColors";
 import BackgroundColors from "./BackgroundColors";
 import Borders from "./Borders";
@@ -13,40 +14,52 @@ import Float from './Float';
 import GridLayout from './GridLayout';
 import Flex from './Flex';
 import ReactIcons from './ReactIcons';
+import BootstrapGrids from './BootstrapGrids';
+import ScreenSizeLabel from './ScreenSizeLabel';
+import BootstrapTables from './BootstrapTables';
+import BootstrapLists from './BootstrapLists';
+import BootstrapForms from './BootstrapForms';
 
 export default function Lab2() {
   return (
-    <div id="wd-css-document-structure">
-      <div className="wd-selector-1">
-        <h3>Document structure selectors</h3>
-        <div className="wd-selector-2">
-          Selectors can be combined to refer elements in particular
-          places in the document
-          <p className="wd-selector-3">
-            This paragraph&apos;s red background is referenced as
-            <br />
-            .selector-2 .selector3<br />
-            meaning the descendant of some ancestor.<br />
-            <span className="wd-selector-4">
-              Whereas this span is a direct child of its parent
-            </span><br />
-              You can combine these relationships to create specific 
-              styles depending on the document structure
-          </p>
+    <Container>
+      <div id="wd-css-document-structure">
+        <div className="wd-selector-1">
+          <h3>Document structure selectors</h3>
+          <div className="wd-selector-2">
+            Selectors can be combined to refer elements in particular
+            places in the document
+            <p className="wd-selector-3">
+              This paragraph&apos;s red background is referenced as
+              <br />
+              .selector-2 .selector3<br />
+              meaning the descendant of some ancestor.<br />
+              <span className="wd-selector-4">
+                Whereas this span is a direct child of its parent
+              </span><br />
+                You can combine these relationships to create specific 
+                styles depending on the document structure
+            </p>
+          </div>
         </div>
+        <ForegroundColors />
+        <BackgroundColors />
+        <Borders />
+        <Padding />
+        <Margins />
+        <Corners />
+        <Dimensions />
+        <Positions />
+        <Zindex />
+        <Float />
+        <GridLayout />
+        <Flex />
+        <ReactIcons />
+        <BootstrapGrids />
+        <ScreenSizeLabel />
+        <BootstrapTables />
+        <BootstrapLists />
+        <BootstrapForms />
       </div>
-      <ForegroundColors />
-      <BackgroundColors />
-      <Borders />
-      <Padding />
-      <Margins />
-      <Corners />
-      <Dimensions />
-      <Positions />
-      <Zindex />
-      <Float />
-      <GridLayout />
-      <Flex />
-      <ReactIcons />
-    </div>
+    </Container>
 );}
