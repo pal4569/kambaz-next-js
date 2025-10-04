3e252590b28e2d919946a949f7788dbcebb7e91f
pal4569 <michaelfc8841@gmail.com>
2025-10-03 23:46:49 -0400

    a2 CSS and Bootstrap

--- app/(Kambaz)/Account/Navigation.tsx
+++ app/(Kambaz)/Account/Navigation.tsx
@@ -1,9 +1,17 @@
 import Link from "next/link";
+
 export default function AccountNavigation() {
- return (
-   <div id="wd-account-navigation">
-     <Link href="Signin"> Signin </Link> <br />
-     <Link href="Signup"> Signup </Link> <br />
-     <Link href="Profile"> Profile </Link> <br />
-   </div>
-);}
+  return (
+    <div id="wd-account-navigation" className="d-flex flex-column">
+      <Link href="/Account/Signin" className="mb-2 text-danger text-decoration-none">
+        Signin
+      </Link>
+      <Link href="/Account/Signup" className="mb-2 text-danger text-decoration-none">
+        Signup
+      </Link>
+      <Link href="/Account/Profile" className="mb-2 text-danger text-decoration-none">
+        Profile
+      </Link>
+    </div>
+  );
+}
--- app/(Kambaz)/Account/Signup/page.tsx
+++ app/(Kambaz)/Account/Signup/page.tsx
@@ -1,13 +1,24 @@
 import Link from "next/link";
+import { FormControl } from "react-bootstrap";
 export default function Signup() {
   return (
     <div id="wd-signup-screen">
       <h3>Sign up</h3>
-      <input placeholder="username" className="wd-username" /><br/>
-      <input placeholder="password" type="password" className="wd-password" /><br/>
-      <input placeholder="verify password"
-             type="password" className="wd-password-verify" /><br/>
-      <Link  href="/Account/Profile" > Sign up </Link><br />
+      <FormControl 
+        placeholder="username" 
+        className="wd-username mb-2" />
+      <FormControl 
+        placeholder="password" 
+        type="password" 
+        className="wd-password mb-2" />
+      <FormControl 
+        placeholder="verify password"
+        type="password" 
+        className="wd-password-verify mb-2" />
+      <Link  
+        id="wd-signout-btn"
+        className="btn btn-primary w-100 mb-2"
+        href="/Account/Profile" > Sign up </Link><br />
       <Link  href="/Account/Signin" > Sign in </Link>
     </div>
 );}
