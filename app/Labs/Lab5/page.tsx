f14e8b6d373cbbfcc41e4c44064eb982b0725223
pal4569 <michaelfc8841@gmail.com>
2025-11-07 18:28:13 -0500

    152 - 161

---
+++ app/Labs/Lab5/page.tsx
@@ -0,0 +1,18 @@
+import PathParameters from "./PathParameters";
+import EnvironmentVariables from "./EnvironmentVariable";
+
+export default function Lab5() {
+  return (
+    <div id="wd-lab5">
+      <h2>Lab 5</h2>
+      <div className="list-group">
+        <a href="http://localhost:4000/lab5/welcome"          
+           className="list-group-item">
+           Welcome
+        </a>
+      </div>
+      <hr/>
+      <EnvironmentVariables />
+      <PathParameters />
+    </div>
+);}
